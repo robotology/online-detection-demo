3d33f5164425ae2ecc73f9cfcfdf3790a96054cf
Arya07 <elisa.maiettini@iit.it>
2018-09-21 20:08:08 +0200

    added buffer for each object tested on server to test on the robot

--- app/python/detections_handler.py
+++ app/python/detections_handler.py
@@ -27,7 +27,7 @@ from random import randint
 yarp.Network.init()
 
 class DetectionsHandler (yarp.RFModule):
-    def __init__(self, input_image_port_name, out_det_img_port_name, input_detections_port_name, rpc_thresh_port_name, image_w, image_h):
+    def __init__(self, input_image_port_name, out_det_img_port_name, input_detections_port_name, rpc_thresh_port_name, out_det_port_name, image_w, image_h):
 
          print 'Setting classes dictionary...\n'
          self._cls2colors = {};
@@ -54,6 +54,10 @@ class DetectionsHandler (yarp.RFModule):
          self._rpc_thresh_port.open(rpc_thresh_port_name)
          print '{:s} opened'.format(self._rpc_thresh_port_name)
 
+         self._out_det_port = yarp.BufferedPortBottle()
+         self._out_det_port_name = out_det_port_name
+         self._out_det_port.open(self._out_det_port_name)
+         print '{:s} opened'.format(self._out_det_port_name)
 
          print 'Preparing input image...\n'
          self._in_buf_array = np.ones((image_h, image_w, 3), dtype = np.uint8)
@@ -67,9 +71,14 @@ class DetectionsHandler (yarp.RFModule):
          self._out_buf_array = np.zeros((image_h, image_w, 3), dtype = np.uint8)
          self._out_buf_image.setExternal(self._out_buf_array, self._out_buf_array.shape[1], self._out_buf_array.shape[0])
 
-         self._old_all_dets = yarp.Bottle()
-         self._old_all_dets.clear()
-         self._counter_old = 0
+
+         print 'Setting detections data...\n'
+         
+         self._old_classes        = {}
+         self._old_bboxes         = {}
+         self._current_detections = yarp.Bottle()
+         # self._current_detections.clear()
+         # self._current_detections = self._out_det_port.prepare()
 
     def _set_label(self, im, text, font, color, bbox):
     	 scale = 0.4
@@ -84,63 +93,92 @@ class DetectionsHandler (yarp.RFModule):
     	 cv2.rectangle(im, label_origin, label_bottom, color, -2)
          cv2.putText(im, text, (int(bbox[0]) + 1, int(bbox[1]) - 5), font, scale, (255,255,255))
 
-    def _drawDetections(self, im, all_dets, thresh=0.15, vis=False):
-            print 'drawDetections*********************************\n'
+    def _pruneOldDetections(self):
+        # Remove from the buffers of old detections all the boxes that are older than 30 frames 
+         print 'pruning old detections'
+         for cls_old in self._old_classes.keys():
+             if self._old_classes[cls_old] >= 10:
+                 del self._old_classes[cls_old]
+                 del self._old_bboxes[cls_old]
+           
+
+    def _checkOldDetectionsAndUpdate(self, all_dets_received):
+         all_dets = all_dets_received
+         if all_dets is not None and not all_dets.get(0).asList().get(0).isString(): #If it's not None and if it's not train command
+             print 'here'
+             # Add to old detections all the new received detections that are not present in the buffers
+	     for j in range(0,all_dets_received.size()):
+	         dets_current = all_dets.get(j).asList()
+	         cls_current  = dets_current.get(5).asString()    
+	         if not cls_current in self._old_classes: 
+	             self._old_classes[cls_current] = 0
+	             self._old_bboxes[cls_current]  = [dets_current.get(0).asDouble(), dets_current.get(1).asDouble(), dets_current.get(2).asDouble(), dets_current.get(3).asDouble(),dets_current.get(4).asDouble()]
+                     print 'After adding:' 
+                     print self._old_classes
+                     print self._old_bboxes
+
+	     # Add to all_dets all the old detections which are not present in all_dets_received 
+	     # Update buffer of old detections if you find a detection that was already present
+	     for cls_old in self._old_classes:
+	         found = False 
+	         for j in range(0,all_dets.size()):
+	             dets_current = all_dets.get(j).asList()
+	             cls_current  = dets_current.get(5).asString()
+	             if cls_old == cls_current: 
+	                 found = True
+                         print 'Found: cls_current: ' + cls_current + ' cls_old: ' + cls_old
+	                 break 
+	         if found:
+	               self._old_classes[cls_old] = 0
+	               self._old_bboxes[cls_old]  = [dets_current.get(0).asDouble(), dets_current.get(1).asDouble(), dets_current.get(2).asDouble(), dets_current.get(3).asDouble(), dets_current.get(4).asDouble()]
+	         else:
+	               self._old_classes[cls_old] = self._old_classes[cls_old] + 1
+	               b = yarp.Bottle
+	               b = all_dets.addList()
+	               b.addDouble(self._old_bboxes[cls_old][0])
+	               b.addDouble(self._old_bboxes[cls_old][1])
+	               b.addDouble(self._old_bboxes[cls_old][2])
+	               b.addDouble(self._old_bboxes[cls_old][3])
+                       b.addDouble(self._old_bboxes[cls_old][4])
+	               b.addString(cls_old)
+             print 'After updating:' 
+             print self._old_classes
+             print self._old_bboxes
+
+         # If received detection is empty but there are old detections in the buffer, populate all_dets with old detections from the buffer
+         elif bool(self._old_classes):
+             print 'HERE'
+             for cls_old in self._old_classes:
+                 self._old_classes[cls_old] = self._old_classes[cls_old] + 1
+                 all_dets = yarp.Bottle()
+	         b = yarp.Bottle()
+	         b = all_dets.addList()
+	         b.addDouble(self._old_bboxes[cls_old][0])
+	         b.addDouble(self._old_bboxes[cls_old][1])
+	         b.addDouble(self._old_bboxes[cls_old][2])
+	         b.addDouble(self._old_bboxes[cls_old][3])
+	         b.addDouble(self._old_bboxes[cls_old][4])
+	         b.addString(cls_old)
+                 print 'After updating when received detection is empty:' 
+                 print self._old_classes
+                 print self._old_bboxes       
+         return all_dets                   
+
+    def _drawDetections(self, im, all_dets_received, thresh=0.15, vis=False):
+            print 'drawDetections*********************************'
+            all_dets = self._checkOldDetectionsAndUpdate(all_dets_received)
             if all_dets is not None:
-            #if not all_dets.size() == 0:
-                    print 'here'
-                    self._old_all_dets = all_dets
 		    for i in range(0,all_dets.size()):
 			dets = all_dets.get(i).asList()
-		        if dets.get(0).isDouble():
+			if dets.get(0).isDouble():
 				bbox = [dets.get(0).asDouble(), dets.get(1).asDouble(), dets.get(2).asDouble(), dets.get(3).asDouble()]  # bbox format: [tl_x, tl_y, br_x, br_y]
 				score = dets.get(4).asDouble()                                                                           # score of i-th detection
 				cls = dets.get(5).asString()                                                                             # label of i-th detection
-			
+		
 				if not cls in self._cls2colors:
 				    new_color = ( randint(0, 255),  randint(0, 255),  randint(0, 255))
 				    self._cls2colors[cls] = new_color
-			
-				# Threshold detections by scores
-				if score >=thresh:
-				    # Draw bounding box for i-th detection
-				    color = self._cls2colors.get(cls)
-				    cv2.rectangle(im,(int(round(bbox[0])), int(round(bbox[1]))),(int(round(bbox[2])), int(round(bbox[3]))),color, 2)
-
-				    # Print text for i-th detection
-				    font = cv2.FONT_HERSHEY_SIMPLEX
-				    text = '{:s} {:.3f}'.format(cls, score)
-				    self._set_label(im, text, font, color, bbox)
-                                    self._counter_old = 0
-
-		        elif dets.get(0).isString() and dets.get(0).asString() == 'train':
-			   for i in range(0,all_dets.size()):
-				ann = all_dets.get(i).asList()
-				bbox = [ann.get(1).asDouble(), ann.get(2).asDouble(), ann.get(3).asDouble(), ann.get(4).asDouble()]  # bbox format: [tl_x, tl_y, br_x, br_y]
-				cls = ann.get(5).asString()                                                                          # label 
-			
-				# Draw bounding box
-				color = (0,0,255)
-				cv2.rectangle(im,(int(round(bbox[0])), int(round(bbox[1]))),(int(round(bbox[2])), int(round(bbox[3]))),color, 2)
-
-				# Print text
-				font = cv2.FONT_HERSHEY_SIMPLEX
-				text = 'Train: {:s}'.format(cls)
-				self._set_label(im, text, font, color, bbox)
-            elif self._old_all_dets is not None:
-                    print 'HERE'
-            	    all_dets = self._old_all_dets
-		    for i in range(0,all_dets.size()):
-			dets = all_dets.get(i).asList()
-		        if dets.get(0).isDouble():
-				bbox = [dets.get(0).asDouble(), dets.get(1).asDouble(), dets.get(2).asDouble(), dets.get(3).asDouble()]  # bbox format: [tl_x, tl_y, br_x, br_y]
-				score = dets.get(4).asDouble()                                                                           # score of i-th detection
-				cls = dets.get(5).asString()                                                                             # label of i-th detection
-			
-				if not cls in self._cls2colors:
-				    new_color = ( randint(0, 255),  randint(0, 255),  randint(0, 255))
-				    self._cls2colors[cls] = new_color
-			
+		
 				# Threshold detections by scores
 				if score >=thresh:
 				    # Draw bounding box for i-th detection
@@ -152,12 +190,13 @@ class DetectionsHandler (yarp.RFModule):
 				    text = '{:s} {:.3f}'.format(cls, score)
 				    self._set_label(im, text, font, color, bbox)
 
-		        elif dets.get(0).isString() and dets.get(0).asString() == 'train':
-			   for i in range(0,all_dets.size()):
-				ann = all_dets.get(i).asList()
+
+			elif dets.get(0).isString() and dets.get(0).asString() == 'train':
+			   for j in range(0,all_dets.size()):
+				ann = all_dets.get(j).asList()
 				bbox = [ann.get(1).asDouble(), ann.get(2).asDouble(), ann.get(3).asDouble(), ann.get(4).asDouble()]  # bbox format: [tl_x, tl_y, br_x, br_y]
 				cls = ann.get(5).asString()                                                                          # label 
-			
+		
 				# Draw bounding box
 				color = (0,0,255)
 				cv2.rectangle(im,(int(round(bbox[0])), int(round(bbox[1]))),(int(round(bbox[2])), int(round(bbox[3]))),color, 2)
@@ -166,10 +205,19 @@ class DetectionsHandler (yarp.RFModule):
 				font = cv2.FONT_HERSHEY_SIMPLEX
 				text = 'Train: {:s}'.format(cls)
 				self._set_label(im, text, font, color, bbox)
-                    self._counter_old = self._counter_old +1
-                    if self._counter_old == 20:
-                        self._old_all_dets.clear()
-                        self._counter_old = 0
+
+                    self._current_detections = self._out_det_port.prepare()
+                    self._current_detections.clear()
+                    self._current_detections.copy(all_dets)
+            else:
+                    self._current_detections = self._out_det_port.prepare()
+                    self._current_detections.clear()
+                    b = self._current_detections.addList()
+                    
+
+            #print 'sending detections...'
+            #self._out_det_port.write()
+
 
             # Return an RGB image with drawn detections
             im=cv2.cvtColor(im,cv2.COLOR_RGB2BGR)
@@ -179,6 +227,12 @@ class DetectionsHandler (yarp.RFModule):
         # Send the result to the image output port
         self._out_buf_array[:,:] = im
         self._out_det_img_port.write(self._out_buf_image)
+
+
+    def _sendDetections(self):
+        print 'sending detections...'
+        self._out_det_port.write()
+     
 
     def _set_threshold(self,cmd, reply):
         print 'setting threshold'
@@ -223,7 +277,7 @@ class DetectionsHandler (yarp.RFModule):
          while(True):
 
             # Read image from port
-            print 'Waiting for image...\n'
+            print '\n\nWaiting for image...\n'
             received_image = self._input_image_port.read()
             print 'Image received...\n'
             self._in_buf_image.copy(received_image)
@@ -236,16 +290,27 @@ class DetectionsHandler (yarp.RFModule):
             detections.clear()
 
             detections = self._input_detections_port.read(False)
+            
+            if detections is not None:
+		    print detections.get(0).asList().get(0).asDouble()
+		    print detections.get(0).asList().get(1).asDouble()
+		    print detections.get(0).asList().get(2).asDouble()
+		    print detections.get(0).asList().get(3).asDouble()
+		    print detections.get(0).asList().get(4).asString()
 
             frame = self._in_buf_array
             frame = cv2.cvtColor(frame,cv2.COLOR_RGB2BGR)
             
             t_draw = time.time()
-
+            # raw_input() #**************************************************************************************************************
             plotted_image = self._drawDetections(frame, detections)
             print 'Time required for drawing on image: %s' % (time.time() - t_draw)
 
-            t_send = time.time()
+            # Update buffers of old detections        
+            t_send = time.time()  
+            self._pruneOldDetections()
+            self._sendDetections()
+
             self._sendDetectedImage(plotted_image)
             print 'Time required for sending detected image: %s' % (time.time() - t_send)
 
@@ -263,6 +328,8 @@ def parse_args():
                         default='/detHandler/detections:i')
     parser.add_argument('--thresh_port', dest='rpc_thresh_port_name', help='rpc port name where to set detection threshold',
                         default='/detHandler:thresh')
+    parser.add_argument('--output_detection_port', dest='out_det_port_name', help='output port for detections',
+                        default='/detHandler:dets:o')
 
     # Image dimensions
     parser.add_argument('--image_w', type=int, dest='image_width', help='width of the images',
@@ -278,7 +345,7 @@ if __name__ == '__main__':
     # Read input parametres
     args = parse_args()
 
-    detHandler = DetectionsHandler(args.input_image_port_name, args.out_det_img_port_name, args.input_detections_port_name, args.rpc_thresh_port_name, args.image_width, args.image_height)
+    detHandler = DetectionsHandler(args.input_image_port_name, args.out_det_img_port_name, args.input_detections_port_name, args.rpc_thresh_port_name, args.out_det_port_name, args.image_width, args.image_height)
 
     #try:
     detHandler.run()
