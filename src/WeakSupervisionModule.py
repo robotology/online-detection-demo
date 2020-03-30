e5bf3f1395dd8ab09ce3d041239a8602f7939c1b
elisa maiettini <elisa.maiettini@iit.it>
2020-03-30 23:30:37 +0200

    added different states

--- src/WeakSupervisionModule.py
+++ src/WeakSupervisionModule.py
@@ -11,36 +11,54 @@ class WeakSupervisionModule(yarp.RFModule):
         self.cmd_port = yarp.Port()
         self.cmd_port.open('/WSModule/command:i')
         print('{:s} opened'.format('/WSModule/command:i'))
-
         self.attach(self.cmd_port)
 
-        return True
+        self._input_image_port = yarp.BufferedPortImageRgb()
+        self._input_image_port.open('/WSModule/image:i')
+        print('{:s} opened'.format('/WSModule/image:i'))
 
-    def cleanup(self):
-        self.cmd_port.close()
-        print('Cleanup function')
+        self._input_predictions_port = yarp.BufferedPortBottle()
+        self._input_predictions_port.open('/WSModule/predictions:i')
+        print('{:s} opened'.format('/WSModule/predictions:i'))
+
+        self.state = 'do_nothing'
+
+        return True
 
     def respond(self, command, reply):
         if command.get(0).asString() == 'refine':
             print('Command refine received')
-            reply.addString('ack')
+            self.state = 'refine'
+            reply.addString('refine state activated')
+        elif command.get(0).asString() == 'stop':
+            self.state = 'do_nothing'
+            reply.addString('refine state deactivated')
         else:
             print('Command {:s} not recognized'.format(command.get(0).asString()))
             reply.addString('nack')
         return True
 
+    def cleanup(self):
+        self.cmd_port.close()
+        self._input_image_port.close()
+        self._input_predictions_port.close()
+        print('Cleanup function')
+
     def interruptModule(self):
         print('Interrupt function')
         self.cmd_port.interrupt()
+        self._input_image_port.interrupt()
+        self._input_predictions_port.interrupt()
         return True
 
     def getPeriod(self):
         return 0.001
 
     def updateModule(self):
-
-
-
+        if self.state == 'do_nothing':
+            pass
+        elif self.state == 'refine':
+            print('refine')
         return True
 
 
