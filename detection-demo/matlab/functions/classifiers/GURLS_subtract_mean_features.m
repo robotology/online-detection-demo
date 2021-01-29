a5ca9c2c80bbc852ece99e22491e0297576f3e61
elisa maiettini <elisa.maiettini@iit.it>
2021-01-29 18:49:20 +0100

    moved app files in unique folder

--- detection-demo/matlab/functions/classifiers/GURLS_subtract_mean_features.m
+++
@@ -1,7 +0,0 @@
-function [ f  ] = GURLS_subtract_mean_features( f, mean_feat )
-
-fprintf('gurls subtraction feature mean\n');
-
-f = f - mean_feat;
-
-end
--- detection-demo/matlab/utils/half_bkg_region.m
+++
@@ -1,4 +0,0 @@
-for i=1:10
-    keys_neg{i} = keys_neg{i}(indx,:);
-    X_neg{i} = X_neg{i}(indx,:);
-end
